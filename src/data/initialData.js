70c0214c7c8d98ec067889f362842a32da387a79
Alex <alexandre_2420@outlook.com>
2022-02-20 14:14:24 -0500

    add: initial setup. install dependecies

---
+++ src/data/initialData.js
@@ -0,0 +1,32 @@
+export const initialData = [
+    {
+        id: 1,
+        character: "Naruto",
+        anime: "Naruto"
+    },
+    {
+        id: 2,
+        character: "Goku",
+        anime: "Dragon Ball"
+    },
+    {
+        id: 3,
+        character: "Kenshin Himura",
+        anime: "Rurouni Kenshin"
+    },
+    {
+        id: 4,
+        character: "Monkey D. Luffy",
+        anime: "One Piece"
+    },
+    {
+        id: 5,
+        character: "Edward Elric",
+        anime: "Fullmetal Alchemist: Brotherhood"
+    },
+    {
+        id: 6,
+        character: "Seto Kaiba",
+        anime: "Yu-Gi-Oh"
+    },
+]
